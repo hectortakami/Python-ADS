2e7184b00f7d4f276207ef1a24aa87134ad4fe0b
Hector M. Takami <hectak97.HT@gmail.com>
2020-02-28 17:43:40 -0600

    Big-O Asymptotic Analysis
    Big O
    
    * Scalability Complexity
    * Space Complexity
    * Tricky Big O Questions

---
+++ Big-O-Asymptotic-Analysis/big-O-scalability.py
@@ -0,0 +1,55 @@
+import time
+import string
+
+print("\n\n\n***************************************\n")
+start = time.time()  # Time start to be meausure
+
+# ------------------------------------------------
+# BIG O - SCALABILITY PATTERNS
+# ------------------------------------------------
+
+# ------------------------------------------------
+# O(1) Complexity -> Constant Time
+# ------------------------------------------------
+# b = []
+# b.append("item")
+# ------------------------------------------------
+
+# ------------------------------------------------
+# O(n) Complexity -> Linear Time
+# ------------------------------------------------
+# a = [1, 2, 3, 4, 5]
+# for i in a:
+#     print(i * 2)
+# ------------------------------------------------
+
+# ------------------------------------------------
+# O(n + m) -> Linear Time
+# ------------------------------------------------
+# a = [1, 2, 3, 4, 5]
+# b = ['1','2','3','4','5']
+# for x in a:
+#     # a as the n input to iterate
+#     print(x)
+# for x in b:
+#     # b as the m input to iterate
+#     print(x)
+#
+# NOTE: They might have the same length but they are
+# AT THE SAME LEVEL OF IDENTATION thats why we add them
+# ------------------------------------------------
+
+# ------------------------------------------------
+# O(n^2) -> Quadratic Time
+# ------------------------------------------------
+# def getAllCombinations(array):
+#     for i in range(0, len(array)):  # O(n)
+#         for j in range(0, len(array)):  # O(n)
+#             print(array[i] + " " + array[j])
+# getAllCombinations(['a', 'b', 'c', 'd', 'e'])
+# NOTE: In NESTED LOOPS we MULTIPLY the number
+# of inputs -> O(n) * O(n) = O(n^2)
+# ------------------------------------------------
+
+# print("\t" + str(time.time() - start) + " seconds")  # Time stops the measurement
+print("\n***************************************\n\n\n")
---
+++ Big-O-Asymptotic-Analysis/big-O-space.py
@@ -0,0 +1,27 @@
+print("\n\n\n***************************************\n")
+# ------------------------------------------------
+# BIG O - SPACE COMPLEXITY PATTERNS
+# ------------------------------------------------
+
+# ------------------------------------------------
+# O(1) Complexity -> Constant Space Allocation
+# ------------------------------------------------
+# def printBoo(n):
+#     for i in n:  # O(1)
+#         print("Boo")
+# printBoo([1, 2, 3, 4, 5])
+# NOTE: We only have one variable assignment
+# (i index in the for loop)
+# ------------------------------------------------
+
+# ------------------------------------------------
+# O(n) Complexity -> Linear Space Allocation
+# ------------------------------------------------
+# def assignHi(n):
+#     array = []  # O(1)
+#     for i in range(0, n):  # O(n)
+#         array.append('hi')  # O(1)
+#     return array
+# print(assignHi(7))
+# ------------------------------------------------
+print("\n***************************************\n\n\n")
